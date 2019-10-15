4ea9412f55194cb1d8eab76ac172ee06e1982740
Kyle Cole <kylepcole@gmail.com>
2019-10-15 23:00:52 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -7,13 +7,13 @@ def fourth_challenge
       :hero_friends => [
         #your hashes here!
         {
-          :name => 'Benvolio',
-          :age => '17',
+          :name => "Benvolio'"
+          :age => "17'"
           :attitude => 'worried'
         },{
-          :name => 'Mercutio',
-          :age => '18',
-          :attitude => 'hot-headed'
+          :name => "Mercutio",
+          :age => "18",
+          :attitude => "hot-headed"
         }
       ]
    }, 
@@ -24,9 +24,9 @@ def fourth_challenge
       :heroine_friends => [
         #your hashes here!
         {
-          :name => 'Steven',
-          :age => '30',
-          :attitude => 'confused'
+          :name => "Steven",
+          :age => "30",
+          :attitude => "confused"
         },
         {
           :name => 'Nurse',
